e37fc4b1bb8500f011192041465eea09bfd68e9f
Admin <igor_lytko@icloud.com>
2021-04-08 12:51:58 +0300

    add dz2.py

---
+++ dz2.py
@@ -0,0 +1,22 @@
+
+#Первая программа
+#value=input("введи целое число:")
+#print(value,type(value))
+#result=int(value) * 2
+#print(result)
+
+
+#Вторая программа
+#value=input("введи целое число:")
+#print(value,type(value))
+#res=float(value)*2
+#print(res)
+
+#Третья программа
+#value=input("введи целое число:")
+#print(value,type(value))
+#res=str(value)*2
+#print(res)
+
+
+
