4c55479e647ebfd5a1235bd6660dff46effc9b27
Admin <igor_lytko@icloud.com>
2021-04-08 14:14:07 +0300

    fix dz2.py

---
+++ testdz3.py
@@ -0,0 +1,31 @@
+www = 'www . google.com'
+if "com" in www:
+    print("com in www")
+else:
+    print("com not in www")
+######################
+
+www ='www. command_and_conquer.com'
+if "com" in www:
+    print("com in www")
+else:
+    print("com not in www")
+################################
+
+www = 'www.command_and_conquer.net'
+if "com" in www:
+    print("com in www")
+else:
+    print("com not in www")
+    #############################
+
+www = 'www.command_and_conquer.net'
+if ".com" in www:
+    print("com in www")
+else:
+    print("com not in www")
+    ####################################
+
+
+
+
